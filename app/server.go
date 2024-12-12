7d834d67d551a7db987fee204f361cf8720563c7
ashraf-kyro <ashraf.khan@kyro.us>
2024-12-12 17:21:04 +0530

    Refactor the logic for parsing the request bytes and creatiing response

--- app/server.go
+++ app/server.go
@@ -1,17 +1,21 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"log"
 	"net"
+	"net/http"
 	"os"
 	"regexp"
+	"strconv"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
 var _ = net.Listen
 var _ = os.Exit
+
+const CRLF = "\r\n"
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -45,31 +49,67 @@ func handleClientConnection(conn net.Conn) {
 	fmt.Printf(`Following data is sent
 %s
 `, string(req[:n]))
-	rawPath, _, _ := bytes.Cut(req[:n], []byte("\r\n"))
-	path := string(rawPath)
-	resp := []byte("HTTP/1.1 404 Not Found\r\n\r\n")
-	if path == "GET / HTTP/1.1" {
-		resp = []byte("HTTP/1.1 200 OK\r\n\r\n")
+	httpRequest := parseHttpRequest(req[:n])
+	httpResponse := HTTPResponse{}
+	httpResponse.httpVersion = httpRequest.httpVersion
+	httpResponse.status = fmt.Sprintf("%d %s", http.StatusNotFound, http.StatusText(http.StatusNotFound))
+	// fmt.Println(httpRequest)
+	if httpRequest.path == "/" {
+		httpResponse.status = fmt.Sprintf("%d %s", http.StatusOK, http.StatusText(http.StatusOK))
 
 	}
-	if found, _ := regexp.MatchString("GET /echo/.* HTTP/1.1", path); found {
+	if strings.HasPrefix(httpRequest.path, "/echo") {
+		httpResponse.status = fmt.Sprintf("%d %s", http.StatusOK, http.StatusText(http.StatusOK))
 		pattern := `/echo/([^ ]+)`
 		re := regexp.MustCompile(pattern)
-		match := re.FindStringSubmatch(path)
+		match := re.FindStringSubmatch(httpRequest.path)
 		// Check if a match is found
 		result := ""
 		if len(match) > 1 {
-			result = match[1]   // Captured group 1
-			fmt.Println(result) // Output: abc
-		} else {
-			fmt.Println("No match found")
+			result = match[1] // Captured group 1
 		}
-		resp = []byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-type: text/plain\r\nContent-length:%d\r\n\r\n%s", len(result), result))
+		httpResponse.body = result
+	}
+	if httpResponse.body != "" {
+		httpResponse.headers["content-type"] = "text/plain"
+		httpResponse.headers["content-lenght"] = strconv.Itoa(len(httpResponse.body))
 	}
 
-	_, err = conn.Write(resp)
+	_, err = conn.Write(getFlattenHttpResponse(httpResponse))
 	if err != nil {
 		log.Fatalf("Error sending data: %v", err)
 	}
 	defer conn.Close()
+}
+
+func parseHttpRequest(req []byte) HttpRequest {
+	httpRequest := HttpRequest{}
+	rawReq := string(req)
+	parts := strings.Split(rawReq, CRLF)
+	// Get the request line
+	reqLineParts := strings.Split(parts[0], " ")
+	httpRequest.method = reqLineParts[0]
+	httpRequest.path = reqLineParts[1]
+	httpRequest.httpVersion = reqLineParts[2]
+
+	// Get the header and parse it
+	headers := map[string]string{}
+	for _, header := range parts[1:] {
+		if header != "" {
+			break
+		}
+		key, value, _ := strings.Cut(header, ":")
+		headers[strings.ToLower(key)] = strings.TrimSpace(value)
+	}
+	httpRequest.headers = headers
+	return httpRequest
+}
+func getFlattenHttpResponse(respStruct HTTPResponse) []byte {
+	flattenHeaders := ""
+	for k, v := range respStruct.headers {
+		flattenHeaders += fmt.Sprintf("%s: %s%s", k, v, CRLF)
+	}
+	resp := fmt.Sprintf("%s %s"+CRLF+"%s"+CRLF+"%s", respStruct.httpVersion, respStruct.status, flattenHeaders, respStruct.body)
+	fmt.Println(resp)
+	return []byte(resp)
 }
